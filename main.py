f39a3e379e6750f097d5ea592b961d6091dd6533
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-10-17 10:12:54 +0200

    zadanie 2

--- main.py
+++ main.py
@@ -28,8 +28,33 @@
 
 #1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
 #wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"
+#
+# first_name=input('Podaj imię:\n')
+# last_name=input('Podaj nazwisko:\n')
+# print(f'Witaj {first_name} {last_name}!')
+# print("Witaj "+first_name+' '+last_name+'!')
+# print("Witaj {} {}!".format(first_name,last_name))
+#
+# liczba=input('podaj liczbę:\n')
+# print(liczba,type(liczba))
+# print(liczba/2)
+
+#print('xyz'*10)
 
 
+#
+# liczba=float(input('podaj liczbę:\n'))
+# print(liczba,type(liczba))
+# print(liczba/2)
 
+#
+#
+# liczba=int(input('podaj liczbę:\n'))
+# print(liczba,type(liczba))
+# print(liczba/2)
 
+#2. • BMI= masa/(wzrost*wzrost) .
+# Napisz program który odbierze od użytkownika jego masę w kilogramach i wzrost w metrach,
+# wyliczy i wypisze BMI.
 
+#print(pow(1.76,2))
