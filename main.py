b1f239956c7347623d226fd81b49c95b28205295
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-10-17 10:03:55 +0200

    zadanie 1 hello world

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 #test
 #Andrzej Klusiewicz
+# print("Hello world")
+# print('Hello world')
+# print("Hello world in Mc'Donalds")
+# zmienna='Kot'
+# zmienna2=1234
+# print(zmienna2)
+# print(type(zmienna2))
+# print("zmienna="+zmienna+"!")
+# #print('zmienna2='+zmienna2)
+# print('zmienna2='+str(zmienna2))
+# x=123
+# y=456
+# print(x,y)
+# print("x="+str(x)+" y="+str(y))
+# print("x={} y={}".format(x,y))
+# print(f"x={x} y={y}")
+# print(f'x={x} y={y}')
+
+# owoc=input('Podaj nazwę owocu:')
+# print(f'Wybrany owoc to {owoc}')
+
+#
+# owoc=input('Podaj nazwę owocu:\n')
+# print(f'Wybrany owoc to {owoc}')
+
+
+#1. Napisz program który przyjmie od użyszkodnika imię oraz nazwisko, a następnie
+#wypisze na konsoli komunikat typu "Witaj TwojeImie TwojeNazwisko!"
+
+
+
+
+
