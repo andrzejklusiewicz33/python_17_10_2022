107417cb59b79f0bc0bd726ae6462345acc81a5e
Andrzej Klusiewicz <andrzejklusiewicz33@gmail.com>
2022-10-18 09:39:25 +0200

    zadanie 34

--- main.py
+++ main.py
@@ -956,3 +956,25 @@
 #33. Wczytaj do słownika dane z pliku dane.csv tak by kluczem było imię sklejone z
 # nazwiskiem rozdzielone spacja, a pozostałe dane znalazły się w wartości
 #   jako krotka lub lista. Przeiteruj po slowniku i wyswietl jego zawartość.
+#
+# slownik=dict()
+# for l in [e.strip().split(';') for e in open('dane.csv',encoding='utf-8')]:
+#     slownik[l[1]+" "+l[2]]=(l[0],l[3],l[4])
+#
+# for k in slownik:
+#     print(k,slownik[k])
+
+#34. Napisz system który zwróci nam ilość wystąpień każdego ze słow w pliku w postaci listy krotek.
+# [  (slowo,ilosc_wystapien),(slowo,ilosc_wystapien)   ]. Nazwa pliku ma zostać przekazana przez zmienną.
+#    Wynik powinien byc posortowany malejąco wg ilosci wystapien
+#    a) odczytaj wszystkie linie z pliku i rozbij na słowa. Każde ze słów dodaj do osobnej listy.
+#       Zadbaj o usunięcie po drodze znaków specjalnych czyli kropek, przecinków, wykrzykników etc.
+#    b) stwórz słownik i dla każdego słowa w liście sprawdz czy istnieje juz wpis dotyczący tego słowa
+#       w słowniku. Jeśli nie ma to dodaj do słownika wpis o kluczu takim jak sprawdzane słowo i wartości 1
+#       dla ilości wystąpień. Jeśli takie słowo pojawia się już w kluczach słownika to trzeba zwiększyc wartośc o 1
+#    c) Przepakuj dane ze słownika do listy i posortuj.
+#
+# Tadeusz
+# Tadeusz,
+# Tadeusz.
+# Tadeusz!
